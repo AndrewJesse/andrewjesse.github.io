89d50430232b68b82c8bf8def3e96212891c67c0
A.J. Marchese <aj.marchese@outlook.com>
2023-03-11 16:39:17 -0800

    added script header

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ Array.from(document.getElementsByClassName("menu-item")).forEach(
     };
   }
 );
+
+// Load header.html using AJAX
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    // Add the meta tags and stylesheets to the head element
+    document.getElementsByTagName("head")[0].innerHTML = this.responseText;
+  }
+};
+xhttp.open("GET", "header.html", true);
+xhttp.send();
